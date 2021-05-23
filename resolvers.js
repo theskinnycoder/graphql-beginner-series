17110097d8f1f2a8bc26f7f7c325bbe08c1703cf
theskinnycoder <18131a0505@gvpce.ac.in>
2021-05-23 20:00:40 +0530

    graphql-apollo-schema

--- resolvers.js
+++ resolvers.js
@@ -1,6 +1,12 @@
+const names = ["Dani", "Mia", "Lana"];
+
 const resolvers = {
   Query: {
-    helloThere: () => "General Kenobi"
+    helloThere: () => "General Kenobi",
+    // sayCheese: () => 5
+    // sayCheese: () => null
+    sayCheese: () => "Cheeeese!",
+    names: () => names
   }
 };
 
--- typeDefs.js
+++ typeDefs.js
@@ -1,8 +1,23 @@
 import { gql } from "apollo-server";
 
 const typeDefs = gql`
+  type Task {
+    id: ID!
+    content: String!
+    isDone: Boolean!
+  }
+
   type Query {
     helloThere: String!
+    sayCheese: String
+    names: [String!]!
+    tasks: [Task!]!
+  }
+
+  type Mutation {
+    newTask(content: String!): Task!
+    updateTask: Task!
+    deleteTask(id: ID!): ID!
   }
 `;
 
